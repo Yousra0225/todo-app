679a5c988adbffbe2895368ef0ba64cbadde6078
Yousra0225 <chbibyousra02@gmail.com>
2025-07-18 01:15:51 +0200

    feat(task-controller): add controller class for task

---
+++ todoApp/src/main/java/yousra/todo_app/demo/controller/TaskController.java
@@ -0,0 +1,35 @@
+package com.example.todobackend.controller;
+
+import org.springframework.web.bind.annotation.*;
+import yousra.todo_app.demo.model.Task;
+import yousra.todo_app.demo.repository.TaskRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/tasks")
+@CrossOrigin(origins = "*")
+public class TaskController {
+
+    private final TaskRepository taskRepo;
+
+    public TaskController(TaskRepository taskRepo) {
+        this.taskRepo = taskRepo;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepo.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepo.save(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable UUID id) {
+        taskRepo.deleteById(id);
+    }
+}
