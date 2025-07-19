79012baaafcd6b859b231c8d43afe874f6f221bc
Yousra0225 <chbibyousra02@gmail.com>
2025-07-19 23:01:04 +0200

    feat(task-app): add principal class

---
+++ todoApp/src/main/java/yousra/todo_app/demo/ToDoBackendApp.java
@@ -0,0 +1,13 @@
+package yousra.todo_app.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoBackendApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoBackendApp.class, args);
+    }
+}
+
