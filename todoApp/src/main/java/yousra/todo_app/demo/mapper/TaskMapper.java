745b862d2d35a109f5a487e2b766cb0634a386ff
Yousra0225 <chbibyousra02@gmail.com>
2025-07-21 12:00:45 +0200

    feat(task-dto): add method to transfer task to dto format

---
+++ todoApp/src/main/java/yousra/todo_app/demo/mapper/TaskMapper.java
@@ -0,0 +1,22 @@
+package yousra.todo_app.demo.mapper;
+
+import yousra.todo_app.demo.dto.TaskDTO;
+import yousra.todo_app.demo.model.Task;
+
+public class TaskMapper {
+    TaskDTO taskDTO = new TaskDTO();
+
+    /**
+     * Method to transfer task to dto task format
+     * @param task model
+     * @return taskDTO task in dto format
+     */
+    public TaskDTO toTaskDTO(Task task){
+        taskDTO.setId(task.getId());
+        taskDTO.setName(task.getTitle());
+        taskDTO.setStatus(task.getStatus());
+        return taskDTO;
+    }
+
+
+}
