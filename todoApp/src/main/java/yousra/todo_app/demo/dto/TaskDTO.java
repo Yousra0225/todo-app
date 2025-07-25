c6c074cf3656736898490ec610e47ccbb7bd2834
Yousra0225 <chbibyousra02@gmail.com>
2025-07-25 21:37:50 +0200

    feat(task-dto): add method to transfer entity to dto format

--- todoApp/src/main/java/yousra/todo_app/demo/dto/TaskDTO.java
+++ todoApp/src/main/java/yousra/todo_app/demo/dto/TaskDTO.java
@@ -5,26 +5,27 @@ import java.util.UUID;
 public class TaskDTO {
     private UUID id;
     private String name;
-    //private boolean status;
+    private boolean status;
 
     /**
      * Getters
      */
     public UUID getId(){return id;}
     public String getName(){return name;}
+    public boolean getStatus(){return status;}
 
     /**
      * Setters
      */
     public void setId(UUID id){this.id = id;}
     public void setName(String name){this.name=name;}
-    //public void setStatus(boolean status){this.status = status;};
+    public void setStatus(boolean status){this.status = status;};
 
     /**
      * Method to check the status of a task
      * @return true if as task is completed and false otherwise
      */
-    //public boolean isCompleted(){return this.status;}
+    public boolean isCompleted(){return this.status;}
 
     public String toString(){
         return "Task{" +
